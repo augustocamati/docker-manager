d671954359e15ebc82379666bdcd770f4206cd45
Augusto Camati <augustocamati21@gmail.com>
2025-10-27 00:47:06 +0100

    feat: implementando o service das imagens

---
+++ src/main/java/camati/docker_manager/services/ImageService.java
@@ -0,0 +1,30 @@
+package camati.docker_manager.services;
+
+import java.util.List;
+
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.model.Image;
+
+import camati.docker_manager.ports.ImageServicePort;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class ImageService implements ImageServicePort {
+
+  private final DockerClient dockerClient;
+  @Override
+  public List<Image> listImages() {
+    return dockerClient.listImagesCmd().exec();
+  }
+
+  @Override
+  public void deleteImage(String id) {
+     dockerClient.removeImageCmd(id).exec();
+  }
+
+  @Override
+  public List<Image> filterImages(String filterName) {
+    return dockerClient.listImagesCmd().withImageNameFilter(filterName).exec();
+  }
+  
+}
