26b7fd5cdbed3b215ce363518ed30789601e7207
Augusto Camati <augustocamati21@gmail.com>
2025-10-27 00:46:33 +0100

    feat: implementando as funcionalidades do service de containers

---
+++ src/main/java/camati/docker_manager/services/ContainerService.java
@@ -0,0 +1,37 @@
+package camati.docker_manager.services;
+
+import java.util.List;
+
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.model.Container;
+
+import camati.docker_manager.ports.ContainerServicePort;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class ContainerService implements ContainerServicePort {
+  private final DockerClient dockerClient;
+
+  @Override
+  public List<Container> listContainers(boolean showAll) {
+    return dockerClient.listContainersCmd()
+        .withShowAll(showAll)
+        .exec();
+  }
+
+  @Override
+  public void startContainer(String id) {
+    dockerClient.startContainerCmd(id).exec();
+  }
+
+  @Override
+  public void stopContainer(String id) {
+    dockerClient.startContainerCmd(id).exec();
+  }
+
+  @Override
+  public void deleteContainer(String id) {
+    dockerClient.removeContainerCmd(id).exec();
+  }
+  
+}
